b52b6cda4692921949e11e2bfc3a8d73b43eaf95
VoXy9132 <voxy9132@gmail.com>
2022-04-27 15:54:38 +0300

    Full mvp

--- LastDream_game.py
+++ LastDream_game.py
@@ -6,47 +6,34 @@ from ctypes import *
 from time import time
 from random import random, choice
 
-#Func
-import pygame.draw
-import pygame.image
+#init
+pg.init()
+width, height = windll.user32.GetSystemMetrics(0), windll.user32.GetSystemMetrics(1) * 0.93
 
-
-#def level_search_options():
-#    file = open('Levels\info.lvi', mode='r')
-#    level = 1
-#    metods = ('Objects')
-#    decor = []
-#    output = dict()
-#    for text in file:
-#        if text.strip().split(':')[0] == 'level':
-#            output['level'] = text.strip().split(':')[1]
-#    file.close()
-#    file = open(f'Levels\level{level}', mode='r')
-#    for met in metods:
-#        output[met] = []
-#        role = False
-#        for text in file:
-#            if text.strip().split(':')[0] == met and len(text.strip().split(':') > 1):
-#                role = True
-#            elif text.strip().split(':')[0] != met and len(text.strip().split(':') <= 1):
-#                break
-#            if role:
-#                temp = text.strip().split(':')[1].strip('[', ']')
-#                for temp_met_opt in temp:
-#                    output[met].append()
-#
-#    return output
-#
-def filterscreen(screen):
-    global width, height
-    n = 150
-    for i in range(0, int(height), int(height / n)):
-        pg.draw.rect(screen, (0, 0, 0), (0, i, width, 1), int(height / n / 1.9))
 def check_pos_in_area(pos_object, pos_area):
     if pos_area[2] > pos_object[0] > pos_area[0] and pos_area[3] > pos_object[1] > pos_area[1]:
         return True
     else:
         return False
+
+def search_the_winner(count):
+    temp_random = choice(range(3))
+    if count == temp_random:
+        return ('not a loser', temp_random)
+    elif count == 0 and temp_random == 2:
+        return ('loser', temp_random)
+    elif count == 2 and temp_random == 0:
+        return ('winner', temp_random)
+    elif count < temp_random and count != 0 and temp_random != 2:
+        return ('winner', temp_random)
+    else:
+        return ('loser', temp_random)
+
+def filterscreen(screen):
+    global width, height
+    n = 150
+    for i in range(0, int(height), int(height / n)):
+        pg.draw.rect(screen, (0, 0, 0), (0, i, width, 1), int(height / n / 1.9))
 
 def frame_display(fps=60):
     pg.time.Clock().tick(fps)
@@ -55,17 +42,6 @@ def frame_display(fps=60):
             pg.quit()
             sys.exit()
     pg.display.flip()
-
-def create_area_sectors(sectors, directions, zip_size):
-    direction_true = []
-    temp_end_sectors_pos = sectors[-1].position
-    for temp_direction in directions:
-        temp_rule = None
-        temp_direction_pos = (10 + sum(list(zip_size)), 10 + sum(list(zip_size)), 10 + sum(list(zip_size)),
-                              10 + sum(list(zip_size)))
-        for temp_sectors in sectors:
-            if Comparision(10 + sum(list(zip_size)), temp_sectors.position_conver):
-                pass
 
 def Comparision(Pos1, Pos2, Diff=1):
     if Pos1[0] + (Diff - 1) * Pos1[2] < Pos2[0] and Pos1[2] + Pos1[0] > Pos2[0] + Pos2[2] * Diff:
@@ -76,30 +52,6 @@ def Comparision(Pos1, Pos2, Diff=1):
 
 
 #Class
-class Battle:
-    def __init__(self, BFI, BFI_attack, npc):
-        self.BattleFrameimage = BFI
-        self.BFI_attack = BFI_attack
-        self.npc_im = npc
-        self.cadr_end = None
-
-    def create(self, screen, screen_size):
-        font = pg.font.Font(pg.font.match_font('impact.ttf'), int(screen_size[1] * 0.075))
-        width, height = screen_size
-        screen.blit(pg.transform.scale(self.BattleFrameimage, (int(width * 0.45), int(height * 0.825))), (int(width * 0.015),
-                                                                                                    int(height * 0.025)))
-        for temp_count_frame_pos in range(len(self.BFI_attack)):
-            screen.blit(pg.transform.scale(self.BFI_attack[temp_count_frame_pos], (int(height * 0.15 * 2),
-                                                                              int((height - height * 0.15) / 5))),
-                        (int(width * 0.47), int(height * 0.025 + (height - height * 0.15) / 5 * temp_count_frame_pos)))
-        BFI_mob_im_tr = pg.transform.scale(self.npc_im.create(), (int(width * 0.35 * (self.npc_im.k)),
-                                                                  int(height * 0.7)))
-        screen.blit(BFI_mob_im_tr, BFI_mob_im_tr.get_rect(center = (int(width * 0.465 / 2), int(height * 0.90 / 2))))
-        screen.blit(font.render(self.npc_im.name, False, (255, 255, 255)), (int(width * 0.045), int(height * 0.06)))
-        if self.cadr_end == None:
-            x = 0
-
-
 class NPC:
     health = 1
     frame = 0
@@ -112,7 +64,7 @@ class NPC:
 
     def create(self):
         self.frame += 1
-        if self.frame >= 7:
+        if self.frame >= 2:
             self.frame = 0
             self.animation += 1
             if self.animation >= 2:
@@ -136,7 +88,6 @@ class Player:
 
     def animation_cadr(self):
         self.cadr += 1
-        print(self.cadr)
         if self.cadr >= 3:
             self.cadr = 0
             self.animation_current[0] += 1
@@ -165,7 +116,6 @@ class Player:
                 self.animation_current[1] = 1
         if self.animation_current[0] >= self.animation_current[1]:
             self.animation_current[0] = 0
-        print(self.animation_current[1])
         temp_pgtransform = pg.transform.scale(self.animation_current[2][self.animation_current[0]],
                                        (int(screen_size[1] * 0.2 * self.k), int(screen_size[1] * 0.2)))
         screen.blit(temp_pgtransform, temp_pgtransform.get_rect(center=(screen_size[0] / 2, screen_size[1] / 2)))
@@ -202,17 +152,16 @@ class Interface:
 
 
 class Gaming:
-    # NPC and Battle load
+    sound_dead_health = pg.mixer.Sound('sounds\hit.wav')
+    sound_dead_mob = pg.mixer.Sound('sounds\Death.wav')
+    sound_dead_player = pg.mixer.Sound('sounds\Game_Over.wav')
     npc = [NPC('Zombie', (400, 400)), NPC('whosthat', (320, 560)), NPC('Big - eared Nanny', (288, 560))]
     BattleFrameimage = pg.image.load(fr'Image\Interface\battle screen.png')
-    BFI_attack = [pg.image.load(fr'Image\Attack\{temp_count_attack}_Attack.png') for temp_count_attack in ['Cut',
-                                                                                                          'Paper',
-                                                                                                          'Stone']]
     money = 0
     background = []
     back_y = 3
     back_x = 3
-    load_image = [pygame.image.load('Image\Objects\glass.png')]
+    load_image = [pg.image.load('Image\Objects\glass.png')]
     for temp_count_back_y in range(back_y):
         for temp_count_back_x in range(back_x):
             background.append([temp_count_back_x, temp_count_back_y])
@@ -224,7 +173,6 @@ class Gaming:
         self.interface = Interface((width, height))
         self.player = Player()
         self.screen_size = (width, height)
-        #self.interface = Interface(screen_size=(width, height))
         temp_background = []
         for temp_back in range(len(self.background)):
             temp_background.append([(self.background[temp_back][0] * (width / (self.back_x - 2))
@@ -264,24 +212,31 @@ class Gaming:
                                                                 int(self.screen_size[1] / (self.back_y - 2)))),
                         temp_back)
         self.player.create(screen, screen_size, self.move)
-        if self.Battle == None and random() >= 0.9 and random() >= 0.8:
-            self.Battle = Battle(self.BattleFrameimage, self.BFI_attack, choice(self.npc))
+        if self.Battle == None and random() >= 0.8 and random() >= 0.8:
+            self.Battle = Battle(self.BattleFrameimage, choice(self.npc), screen_size)
+            pg.mixer.music.load('sounds\Battle.wav')
+            pg.mixer.music.play()
         elif self.Battle != None:
             temp_result = self.Battle.create(screen, screen_size)
-            if temp_result == 'end':
+            if temp_result == 'win':
+                self.player.health = self.player.health_max
                 self.Battle = None
+                self.sound_dead_mob.play()
+                pg.mixer.music.load('sounds\Music.wav')
+                pg.mixer.music.play()
+            elif temp_result == 'defeat':
+                self.player.health -= 1
+                self.sound_dead_health.play()
+                if self.player.health == 0:
+                    self.sound_dead_player.play(maxtime=2500)
+                    pg.mixer.music.load('sounds\Music.wav')
+                    pg.mixer.music.play()
+                    self.Battle = None
+                    self.player.health = self.player.health_max
         self.interface.create(screen, self.player.health)
 
-
-class View:
-    pass
-class Sectors:
-    def __init__(self, position):
-        self.position = position
-        self.position_conver = (position[0], position[0], position[0] - position[0], position[0] - position[0])
-
-
 class Button:
+    clamp_sound = pg.mixer.Sound(r'sounds\button.wav')
     colors = {'clamp': (255, 100, 100), 'standart': False, 'clamp_off': (255, 255, 255)}
     def __init__(self, image, position, highlighting=False):
         position = [int(temp) for temp in position]
@@ -307,34 +262,73 @@ class Button:
                 self.color = self.colors['clamp_off']
                 if pg.mouse.get_pressed()[0]:
                     self.color = self.colors['clamp']
+                    self.clamp_sound.play()
                     return True
             else:
                 self.color = self.colors['standart']
             return False
         else:
             if check_pos_in_area(pg.mouse.get_pos(), event_position):
+                for event in pg.event.get():
+                    if event.type == pg.MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            self.clamp_sound.play()
+                            return True
                 if pg.mouse.get_pressed()[0]:
+                    self.clamp_sound.play()
                     return True
             return False
+class Battle:
+    attacks_name = ('cut', 'paper', 'stone')
+    attacks_frame = None
+    text_winner_rule = None
+    color_took = (255, 0, 0)
+    def __init__(self, BFI, npc, screen_size):
+        width, height = screen_size
+        self.BattleFrameimage = BFI
+        self.npc_im = npc
+        self.cadr_end = None
+        self.BFI_attack = [Button(pg.image.load(fr'Image\Attack\{temp_image_name}_Attack.png'),
+                                  (int(width * 0.47), int(height * 0.025 + (height - height * 0.15) / 5 * temp_count),
+                                   (int(height * 0.15 * 2)),
+                                    int((height - height * 0.15) / 5)
+                                  )) for temp_image_name, temp_count in (('Cut', 0), ('Paper', 1), ('Stone', 2))]
 
-class Generation_world:
-    sectors_count = 25
-    sectors_directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-    sectors_add_size = 5
-    sectors_min_size = 5
-    zip_sectors_size = (sectors_min_size, sectors_add_size)
-    def __init__(self):
-        pass
 
-    def generate(self):
-        sectors = [Sectors((0, 0, int(self.sectors_min_size + random() * self.sectors_add_size),
-                            int(self.sectors_min_size + random() * self.sectors_add_size)))]
-        for temp_sectors in range(self.sectors_count - 1):
-            sectors.append(create_area_sectors(sectors, self.sectors_directions, zip_sectors_size))
+    def create(self, screen, screen_size):
+        font = pg.font.Font(pg.font.match_font('impact.ttf'), int(screen_size[1] * 0.075))
+        width, height = screen_size
+        screen.blit(pg.transform.scale(self.BattleFrameimage, (int(width * 0.45), int(height * 0.825))), (int(width * 0.015),
+                                                                                                    int(height * 0.025)))
+        for BFI_att in self.BFI_attack:
+            BFI_att.create(screen)
+        BFI_mob_im_tr = pg.transform.scale(self.npc_im.create(), (int(width * 0.35 * (self.npc_im.k)),
+                                                                  int(height * 0.7)))
+        screen.blit(BFI_mob_im_tr, BFI_mob_im_tr.get_rect(center = (int(width * 0.465 / 2), int(height * 0.90 / 2))))
+        screen.blit(font.render(self.npc_im.name, False, (255, 255, 255)), (int(width * 0.045), int(height * 0.06)))
+        if self.cadr_end == None:
+            x = 0
+        for temp_click_attack in range(len(self.BFI_attack)):
+            if self.BFI_attack[temp_click_attack].event():
+                self.text_winner_rule = search_the_winner(temp_click_attack)
+                self.color_took = (255, 0, 0)
+        if self.text_winner_rule != None:
+            screen.blit(font.render(fr'Mob took the {self.attacks_name[self.text_winner_rule[1]]}', False,
+                                    self.color_took), (int(width * 0.75), int(height * 0.06)))
+            if self.color_took == (255, 0, 0):
+                self.color_took = (255, 255, 255)
+            if self.text_winner_rule[0] == 'winner':
+                self.text_winner_rule = None
+                return 'win'
+            elif self.text_winner_rule[0] == 'loser':
+                self.text_winner_rule = None
+                return  'defeat'
+        self.text_winner_rule = None
+        return None
 
-#init
-pg.init()
-width, height = windll.user32.GetSystemMetrics(0), windll.user32.GetSystemMetrics(1) * 0.93
+
+
+
 #width, height = 800, 600
 
 #image_load
@@ -356,6 +350,8 @@ pg.display.set_icon(pg.image.load('Image\Window\icon.png'))
 screen = pg.display.set_mode((width, height))
 main_menu = True
 game_role = False
+pg.mixer.music.load('sounds\Music.wav')
+pg.mixer.music.play()
 while True:
     game = Gaming(width, height)
     while main_menu:
